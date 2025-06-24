f2553900487ee8785bcc7f08a0e8af44a89537f5
Aaditya Sigdel <adityasigdel8@gmail.com>
2025-06-24 11:43:45 +0545

    fetchDataFromApi

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react"
 
 export default function FetchData() {
-  
-  return (
-    <>
-    </>
-  )
+
+  const [apidata,setapidata] = useState([]);
+
+    const HandleFetch = async ()=>{
+      try {
+      const apiresult = await fetch("https://mocki.io/v1/5a70996f-a789-4e3e-b7c2-fe76be4fa909")
+      const result = await apiresult.json()
+      setapidata(result)
+      console.log(result)
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+      useEffect (()=>{
+       HandleFetch(); 
+        const intervalId = setInterval(HandleFetch, 5000); 
+
+    return () => clearInterval(intervalId); 
+    },[])
+
+
+  return null;
+
 }
