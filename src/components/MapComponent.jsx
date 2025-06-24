eae779fd0a247c6b77403c2c0998dca9667c31d5
Aaditya Sigdel <adityasigdel8@gmail.com>
2025-06-24 16:39:41 +0545

    Marker

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -1,32 +1,63 @@
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
-import {contextData} from '../ContextApi/Context'
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import { contextData } from '../ContextApi/Context'
 import { useContext } from "react";
 
 export default function MapComponent() {
-  const {apidata, setapidata} = useContext(contextData);
+  const { apidata, setapidata } = useContext(contextData);
 
   const DefaultStyle = {
     width: "100%",
     height: "80vh"
   }
 
-  const defaultCenter = {
-    lat: 27.7172,
-    lng: 85.3240
-  }
+  const defaultCenter = apidata.length > 0
+    ? {
+      lat: parseFloat(apidata[0].latitude),
+      lng: parseFloat(apidata[0].longitude)
+    }
+    : {
+      lat: 27.7172,
+      lng: 85.3240
+    };
 
-     console.log("Fetched Data:", apidata);
+  console.log("Fetched Data:", apidata);
+
+
 
   return (
     <>
-      <LoadScript googleMapsApiKey="AIzaSyAmhPPLqzBFkyrkmaimjjKnFFLRd2SqzZI">
-        <GoogleMap
-          center={defaultCenter}
-          mapContainerStyle={DefaultStyle}
-          zoom={15}
-        />
-      </LoadScript>
+      <div className="felx border-3 border-black rounded-xl ">
+        <LoadScript googleMapsApiKey="AIzaSyAmhPPLqzBFkyrkmaimjjKnFFLRd2SqzZI">
+          <GoogleMap
+            center={defaultCenter}
+            mapContainerStyle={DefaultStyle}
+            zoom={15}
 
+
+          >
+            {apidata.map((item) => (
+              <Marker
+                key={item.riderId}
+                position={{ lat: item.latitude, lng: item.longitude }}
+                label={{
+                  text: item.name,
+                  color: 'white',
+                  fontWeight: "bold",
+                  fontSize: "14px"
+
+                }
+                }
+                icon={{
+                  url: "https://www.svgrepo.com/show/122485/car-placeholder.svg",
+                  scaledSize: new window.google.maps.Size(40, 40)
+                }}
+              />
+            ))}
+          </GoogleMap>
+
+
+        </LoadScript>
+      </div>
     </>
   );
 }
