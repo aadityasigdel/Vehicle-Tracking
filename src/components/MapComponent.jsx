addf4ed494fc56ba03be7168674c2d8de79fa5de
Aaditya Sigdel <adityasigdel8@gmail.com>
2025-06-24 11:30:13 +0545

    MapLoaded

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -1,12 +1,26 @@
-
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
 
 export default function MapComponent() {
-return(
-  <>
-     <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  
-  </>
+  const DefaultStyle = {
+    width: "100%",
+    height: "80vh"
+  }
+
+  const defaultCenter = {
+    lat: 27.7172,
+    lng: 85.3240
+  }
+
+  return (
+    <>
+      <LoadScript googleMapsApiKey="AIzaSyAmhPPLqzBFkyrkmaimjjKnFFLRd2SqzZI">
+        <GoogleMap
+          center={defaultCenter}
+          mapContainerStyle={DefaultStyle}
+          zoom={15}
+        />
+      </LoadScript>
+
+    </>
   );
 }
