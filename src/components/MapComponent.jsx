2b408cb21b345ac770b4724555a121cc49ea64a3
Aaditya Sigdel <adityasigdel8@gmail.com>
2025-06-25 10:47:32 +0545

    Fixedstyle

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -1,17 +1,25 @@
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
-import { contextData } from '../ContextApi/Context'
+import { GoogleMap, LoadScript, Marker, Polyline } from "@react-google-maps/api";
+import { contextData } from '../ContextApi/Context';
 import { useContext } from "react";
-import Bike from "../assets/Bike.jpg"
-import Car from '../assets/car.jpg'
+import Bike from "../assets/Bike.jpg";
+import Car from '../assets/car.jpg';
+import { useState, useEffect } from "react";
 
 export default function MapComponent() {
+
+  //Access Data from Api
   const { apidata, setapidata } = useContext(contextData);
 
+  //Stores location data for prolyline
+  const [LocationData, setLocationData] = useState([]);
+
+  // Map Height and Width
   const DefaultStyle = {
     width: "100%",
     height: "80vh"
   }
 
+  //Map centred according to Api Location (Adjust to KTM if no Data found)
   const defaultCenter = apidata.length > 0
     ? {
       lat: parseFloat(apidata[0].latitude),
@@ -22,13 +30,41 @@ export default function MapComponent() {
       lng: 85.3240
     };
 
+  //Stores location data and Rider data from api to create ProlyLine
+  useEffect(() => {
+    if (apidata.length > 0) {
+      setLocationData((prev) => {
+        const updated = { ...prev };
+
+        apidata.forEach((rider) => {
+          const newPoint = {
+            lat: parseFloat(rider.latitude),
+            lng: parseFloat(rider.longitude),
+          };
+
+          const id = rider.riderId;
+
+          if (!updated[id]) {
+            updated[id] = [];
+          }
+
+          updated[id].push(newPoint);
+        });
+        return updated;
+      });
+    }
+  }, [apidata]);
+
+
   console.log("Fetched Data:", apidata);
 
 
 
   return (
     <>
-      <div className="felx border-3 border-black rounded-xl ">
+      <div className="flex border-t-3 border-r-3 border-l-3 border-black rounded-t-xl ">
+
+        {/*Loads the Google Map Component */}
         <LoadScript googleMapsApiKey="AIzaSyAmhPPLqzBFkyrkmaimjjKnFFLRd2SqzZI">
           <GoogleMap
             center={defaultCenter}
@@ -37,6 +73,7 @@ export default function MapComponent() {
 
 
           >
+            {/*Loads Marker Data from Api*/}
             {apidata.map((item) => (
               <Marker
                 key={item.riderId}
@@ -51,17 +88,34 @@ export default function MapComponent() {
                 }
                 icon={{
                   url: item.categoryId === 1
-                    ? {Bike}
-                    : {Car}, 
-                  scaledSize: new window.google.maps.Size(40, 40),
+                    ? { Bike }
+                    : { Car },
+                  scaledSize: new window.google.maps.Size(60, 60),
                 }}
 
               />
             ))}
+
+          {/*Creates ProlyLine from stored data */}
+            {Object.entries(LocationData).map(([riderId, path]) => (
+              <Polyline
+                key={riderId}
+                path={path}
+                options={{
+                  strokeColor: "#FF0000",
+                  strokeOpacity: 1.0,
+                  strokeWeight: 2,
+                }}
+              />
+            ))}
           </GoogleMap>
-
-
         </LoadScript>
+      </div>
+      <div className="bg-gray-100 p-4 text-sm  border-b-3 border-r-3 border-l-3 border-black rounded-b-xl">
+        <h3 className="font-semibold mb-2">Live Vehicle Info</h3>
+        <ul className="space-y-1">
+
+        </ul>
       </div>
     </>
   );
--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,25 +1,29 @@
-import { useContext, useEffect, useState } from "react"
+import { useContext, useEffect } from "react"
 import { contextData } from "../ContextApi/Context"
 
 export default function FetchData() {
 
-  const {apidata, setapidata} = useContext(contextData)
+  //stores Api Data
+  const { apidata, setapidata } = useContext(contextData);
 
-    const HandleFetch = async ()=>{
-      try {
+  //Fetch data from Api and Stors it in apidata
+  const HandleFetch = async () => {
+    try {
       const apiresult = await fetch("http://185.193.19.5/server/api/v1/recent-location-by-province?province=Bagamati%20Province")
       const result = await apiresult.json()
       setapidata(result)
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-
+    } catch (error) {
+      console.error("Error fetching data:", error);
     }
-      useEffect (()=>{
-       HandleFetch(); 
-        const intervalId = setInterval(HandleFetch, 5000); 
-    return () => clearInterval(intervalId); 
-    },[])
+
+  }
+
+  //Runs every 5sec to update data from API
+  useEffect(() => {
+    HandleFetch();
+    const intervalId = setInterval(HandleFetch, 5000);
+    return () => clearInterval(intervalId);
+  }, [])
   return null;
 
 }
