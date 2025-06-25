6c05ab8660c8aa93a79d9690fd476a9fe297911d
Aaditya Sigdel <aadityasigdel953@gmail.com>
2025-06-25 16:47:13 +0545

    fixed:Warnings

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -9,7 +9,7 @@ export default function FetchData() {
   //Fetch data from Api and Stors it in apidata
   const HandleFetch = async () => {
     try {
-      const apiresult = await fetch("http://192.168.100.178:5000/api/vehicles");
+      const apiresult = await fetch("https://6e7d-2407-54c0-1b15-b0e2-598a-b787-9447-4f4b.ngrok-free.app/api/vehicles");
       const result = await apiresult.json()
       setapidata(result)
     } catch (error) {
